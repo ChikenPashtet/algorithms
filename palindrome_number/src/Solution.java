870a069e95f7fa29f7a0bd8c6201aa34aaa2e411
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-02-17 23:39:04 +0700

    feat: palindrome number

---
+++ palindrome_number/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String number = String.valueOf(x);
+        int length = number.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (number.charAt(i) != number.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
