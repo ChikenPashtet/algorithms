a133315c0f242d04131c49782c1227f43e307377
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-02-19 21:45:31 +0700

    feat: concatenation of array

---
+++ concatenation_of_array/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int length = nums.length;
+        int[] arr = new int[length * 2];
+        for (int i = 0; i < length; i++) {
+            arr[i] = nums[i];
+            arr[i + length] = nums[i];
+        }
+        return arr;
+    }
+}
