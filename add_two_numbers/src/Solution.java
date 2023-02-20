d2e7a95876c04afb7af36c16cdb8cb90c8118e4e
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-02-20 22:24:41 +0700

    feat: add two numbers

---
+++ add_two_numbers/src/Solution.java
@@ -0,0 +1,52 @@
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode listNode = addTwoNumbers(new ListNode(5, new ListNode(4, new ListNode(9))), new ListNode(5, new ListNode(6)));
+        System.out.println(listNode.val);
+        while (listNode.next != null) {
+            listNode = listNode.next;
+            System.out.println(listNode.val);
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            current.next = node;
+            current = current.next;
+        }
+        return result.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
