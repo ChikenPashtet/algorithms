c723db98a342bdd91015efafdd6762eee605ca05
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-02-23 22:59:57 +0700

    feat: how many numbers are smaller than the current

---
+++ how_many_numbers_are_smaller_than_the_current_number/src/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101];
+        int[] res = new int[nums.length];
+
+        for (int i =0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+
+        for (int i = 1 ; i <= 100; i++) {
+            count[i] += count[i-1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                res[i] = 0;
+            else
+                res[i] = count[nums[i] - 1];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        smallerNumbersThanCurrent(new int[]{6,5,4,8});
+    }
+}
