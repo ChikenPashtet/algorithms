05ac40187a7340241fa1c95c57d4462cbdd4952a
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-02-24 15:19:27 +0700

    feat: 3Sum

---
+++ 3sum/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[l]);
+                    temp.add(nums[r]);
+                    result.add(temp);
+                }
+                if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
