1b8fe280713495d55818114d8d5a7fc7f00c6402
Pavel Stalev <stalev.pavel1999@icloud.com>
2023-03-13 02:44:11 +0700

    feat: letter combinations of a phone number

--- letter_combinations_of_a_phone_number/src/Solution.java
+++ letter_combinations_of_a_phone_number/src/Solution.java
@@ -1,20 +1,28 @@
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
-    public List<String> letterCombinations(String digits) {
-        HashMap<Integer, Character[]> map = new HashMap<>();
+    public static List<String> letterCombinations(String digits) {
+        LinkedList<String> queue = new LinkedList<>();
+        String[] array = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits.isEmpty()) return queue;
+        queue.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int digit_button = Character.getNumericValue(digits.charAt(i));
+            while (queue.peek().length() == i) {
+                String last = queue.remove();
+                for (char c : array[digit_button].toCharArray()) {
+                    queue.add(last + c);
+                }
+            }
+        }
+        return queue;
+    }
 
-        map.put(2,new Character[]{'a','b','c'});
-        map.put(3,new Character[]{'d','e','f'});
-        map.put(4,new Character[]{'g','h','i'});
-        map.put(5,new Character[]{'j','k','l'});
-        map.put(6,new Character[]{'m','n','o'});
-        map.put(7,new Character[]{'p','q','r','s'});
-        map.put(8,new Character[]{'t','u','v'});
-        map.put(9,new Character[]{'w','x','y','z'});
-
-
+    public static void main(String[] args) {
+        List<String> list = letterCombinations("23");
+        for (String i : list) {
+            System.out.print(i + "; ");
+        }
     }
 
 }
